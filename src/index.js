321510ea87676fec448c33a303f1d74129cfa036
mustafa2036 <missem704@gmail.com>
2022-09-28 12:55:51 +0200

    Chnages upload new files

--- src/index.js
+++ src/index.js
@@ -6,14 +6,14 @@ import 'bootstrap/dist/js/bootstrap.bundle.js';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter} from 'react-router-dom'
+import {HashRouter} from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
     <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
 
